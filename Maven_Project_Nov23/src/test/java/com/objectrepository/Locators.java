17c9f5c3eedb641b4f3007d452d772a27ac46153
Richa Sonawane <richa_deore@yahoo.in>
2023-11-28 17:54:48 -0600

    Implemented OR and testdata file

--- Maven_Project_Nov23/src/test/java/com/objectrepository/Locators.java
+++ Maven_Project_Nov23/src/test/java/com/objectrepository/Locators.java
@@ -1,5 +1,18 @@
 package com.objectrepository;
 
+import org.openqa.selenium.By;
+
 public class Locators {
 
+	//public  final By ELEMENTNAME_ELEMENTTYPE = By.id("attribute");
+	
+	//locators for fb
+	public final By USERNAME_EDITBOX = By.id("email");
+	public final By PASSWORD_EDITBOX = By.name("pass");
+	public final By LOGIN_BUTTON = By.name("login");
+	
+	//locator for google
+	public final By SEARCH_EDITBOX = By.name("q");
+	public final By SEARCH_BUTTON = By.name("btnK");
+	
 }
---
+++ Maven_Project_Nov23/src/test/java/com/testscenario/FB_Login.java
@@ -0,0 +1,104 @@
+package com.testscenario;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.objectrepository.Locators;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FB_Login {
+	WebDriver driver;
+	// create object of locator class
+	Locators obj = new Locators();
+
+	@Test
+	public void Fb_login() throws IOException {
+		// create property file variable to call test data from property file
+		FileInputStream path = new FileInputStream(".\\TestData\\testdata.properties");
+		Properties prop = new Properties();
+		prop.load(path);
+
+		driver.get(prop.getProperty("baseURL"));
+		driver.findElement(obj.USERNAME_EDITBOX).sendKeys(prop.getProperty("Username"));
+		driver.findElement(obj.PASSWORD_EDITBOX).sendKeys(prop.getProperty("Password"));
+		driver.findElement(obj.LOGIN_BUTTON).click();
+
+	}
+
+	@Test
+	public void google_login() throws IOException {
+		// for every test we need to call testdata from properties file
+		FileInputStream path = new FileInputStream(".\\TestData\\testdata.properties");
+		Properties prop = new Properties();
+		prop.load(path);
+
+		driver.get("https://www.google.com/");
+		driver.findElement(obj.SEARCH_EDITBOX).sendKeys(prop.getProperty("SearchText"));
+		driver.findElement(obj.SEARCH_BUTTON).click();
+
+	}
+	@Parameters("browserName")
+	@BeforeClass
+	public void beforeClass(@Optional("Chrome") String browserName) {
+
+		if (browserName.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (browserName.equalsIgnoreCase("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		} else if (browserName.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else {
+			System.out.println("select browser correctly");
+		}
+
+		// If needed exact version then do
+		// WebDriverManager.chromedriver().browserVersion("84").setup();
+
+		driver.manage().window().maximize();
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+	@AfterMethod
+	public void takeScreenshot() throws IOException {
+		// date time in customised format
+		Date d = new Date();
+		DateFormat df = new SimpleDateFormat("ddMMyyyy_HHmmss");
+		String timeStamp = df.format(d);
+		System.out.println(timeStamp);
+
+		// Take screenshot
+		File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		// FileHandler.copy(srcFile, new File(".\\Screenshots\\FB"+timeStamp+".PNG"));
+		FileHandler.copy(srcFile, new File(".\\Screenshots\\FB" + timeStamp + ".PNG"));
+
+	}
+
+}
